4723f82c875b745233e2921abcb991451109ffcb
joloLG <jlgracilla@sorsu.edu.ph>
2025-10-04 19:18:56 +0800

    Access Token (For endpoint Testing)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             IncidentTypeSeeder::class,
+            AccessTokenSeeder::class,
         ]);
     }
 }
