dfe6a853872b800e5fab9c7191a17103557f8a6f
joloLG <jlgracilla@sorsu.edu.ph>
2025-10-04 19:38:07 +0800

    User Authentication for Registration on Postman (register api)

---
+++ database/seeders/IncidentSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Incident;
+use Illuminate\Database\Seeder;
+
+class IncidentSeeder extends Seeder
+{
+         public function run(): void {
+
+         }
+}
