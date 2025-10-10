127c9c8156afb66d1c6fa8898a9fb257e6bf180a
AldrinGonzales <aldrin.gonzales@sorsu.edu.ph>
2025-10-10 13:47:59 +0800

    Update the IncidentTypeSeeder and the IncidentTypeFactory

--- database/seeders/IncidentTypeSeeder.php
+++ database/seeders/IncidentTypeSeeder.php
@@ -9,42 +9,7 @@ class IncidentTypeSeeder extends Seeder
 {
     public function run(): void
     {
-        $types = [
-            [
-                'name' => 'Fire Emergency(Sunog)',
-                'description' => 'Incidents involving fire breakouts or smoke sightings.',
-            ],
-            [
-                'name' => 'Medical Emergency',
-                'description' => 'Injuries, illnesses, or medical-related incidents requiring assistance.',
-            ],
-            [
-                'name' => 'Natural Disaster',
-                'description' => 'Floods, earthquakes, typhoons, and other weather-related incidents.',
-            ],
-            [
-                'name' => 'Vehicular Accident',
-                'description' => '2 or more Vehicle collisions or breakdowns, or collision with person.',
-            ],
-            [
-                'name' => 'Power Outage',
-                'description' => 'Loss of electrical power in an area or facility.',
-            ],
-            [
-                'name' => 'Public Disturbance',
-                'description' => 'Riots, protests, or large gatherings causing disruption.',
-            ],
-            [ 
-                'name' => 'Other',
-                'description' => 'Incidents that do not fit into the predefined categories.',
-            ],
-        ];
-
-        foreach ($types as $type) {
-            IncidentType::updateOrCreate(
-                ['name' => $type['name']],
-                $type
-            );
-        }
+        // Create 7 random incident types
+        IncidentType::factory()->count(7)->create();
     }
 }
